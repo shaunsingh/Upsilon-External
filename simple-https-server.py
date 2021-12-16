33fa47caff96c6b7eb66fb6a89accdb530820b5f
Yaya-Cout <67095734+Yaya-Cout@users.noreply.github.com>
2021-12-16 16:12:53 +0100

    Update to python3 (#4)

--- simple-https-server.py
+++ simple-https-server.py
@@ -2,13 +2,15 @@
 # generate server.xml with the following command:
 #    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 # run as follows:
-#    python simple-https-server.py
+#    python3 simple-https-server.py
 # then in your browser, visit:
 #    https://localhost:4443
 
-import BaseHTTPServer, SimpleHTTPServer
+import http.server
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 8443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd = http.server.HTTPServer(('0.0.0.0', 8443),
+                               http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.pem',
+                               server_side=True)
 httpd.serve_forever()
